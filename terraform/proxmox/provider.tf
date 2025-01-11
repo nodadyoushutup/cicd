3301554ed04ab3e3ca6c1d781d8567654740d880
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-10 19:44:57 -0500

    [WIP] Proxmox

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   required_providers {
-#     proxmox = {
-#       source = "bpg/proxmox"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
 
-# provider "proxmox" {
-#   endpoint = var.PROXMOX_VE_ENDPOINT
-#   password = var.PROXMOX_VE_PASSWORD
-#   username = var.PROXMOX_VE_USERNAME
-#   random_vm_ids = true
-#   insecure = true
-# }
+provider "proxmox" {
+  endpoint = var.PROXMOX_VE_ENDPOINT
+  password = var.PROXMOX_VE_PASSWORD
+  username = var.PROXMOX_VE_USERNAME
+  random_vm_ids = true
+  insecure = true
+}

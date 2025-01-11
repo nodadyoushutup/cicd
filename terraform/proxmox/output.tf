bc78a8fd3610f39af9f130332773e53ea81779c2
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-10 19:33:58 -0500

    [WIP] Proxmox

--- terraform/proxmox/output.tf
+++ terraform/proxmox/output.tf
@@ -3,5 +3,5 @@ data "proxmox_virtual_environment_user" "root" {
 }
 
 output "root" {
-  value = data.proxmox_virtual_environment_user.user_id
+  value = data.proxmox_virtual_environment_user.root.user_id
 }

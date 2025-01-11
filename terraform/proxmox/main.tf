433e0272c327f36e505df4069d1d594fdf6f4be1
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-11 17:25:42 -0500

    [WIP] Proxmox

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,16 +1,3 @@
-data "local_file" "ssh_public_key" {
-  filename = var.GITCONFIG
-}
-
-data "local_file" "ssh_private_key" {
-  filename = var.SSH_PRIVATE_KEY
-}
-
-data "local_file" "gitconfig" {
-  filename = var.SSH_PUBLIC_KEY
-}
-
-
 resource "proxmox_virtual_environment_file" "cicd_cloud_config" {
   content_type = "snippets"
   datastore_id = "local"

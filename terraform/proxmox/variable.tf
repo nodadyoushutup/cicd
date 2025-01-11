042fb3cf6e7ee8a4f724bb510c9e3e556cf7eec0
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-10 19:40:54 -0500

    [WIP] Proxmox

--- terraform/proxmox/variable.tf
+++ terraform/proxmox/variable.tf
@@ -1,53 +1,14 @@
-# variable "CONFIG_PATH" {
-#   type = string
-#   description = "Configuration path"
-#   default = "/mnt/workspace/source/terraform/spacelift/config/config.yaml"
-# }
+variable "PROXMOX_VE_USERNAME" {
+  type = string
+  default = null
+}
 
-# variable "DEFAULT_PRIVATE_KEY" {
-#   type = string
-#   description = "Default private key"
-#   default = "/mnt/workspace/id_rsa"
-# }
+variable "PROXMOX_VE_PASSWORD" {
+  type = string
+  default = null
+}
 
-# variable "DEFAULT_PUBLIC_KEY_DIR" {
-#   type = string
-#   description = "Public SSH keys directory"
-#   default = "/mnt/workspace/"
-# }
-
-# variable "DEFAULT_USERNAME" {
-#   type = string
-#   description = "Default username"
-#   default = null
-# }
-
-# variable "DEFAULT_PASSWORD" {
-#   type = string
-#   description = "Default password"
-#   default = null
-# }
-
-# variable "DEFAULT_IP_ADDRESS" {
-#   type = string
-#   description = "Default public IP address"
-#   default = null
-# }
-
-# variable "DEFAULT_GATEWAY" {
-#   type = string
-#   description = "Default internal gateway address"
-#   default = null
-# }
-
-# variable "GITHUB_BRANCH" {
-#   type = string
-#   description = "Github branch"
-#   default = null
-# }
-
-# variable "GITHUB_REPOSITORY" {
-#   type = string
-#   description = "Github repository"
-#   default = null
-# }
+variable "PROXMOX_VE_ENDPOINT" {
+  type = string
+  default = null
+}

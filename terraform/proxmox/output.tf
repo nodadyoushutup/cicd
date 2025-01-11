d06ef13d3af74d77d81eca02d14bece7673c15af
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-10 21:36:59 -0500

    [WIP] Proxmox

--- terraform/proxmox/output.tf
+++ terraform/proxmox/output.tf
@@ -1,5 +1,3 @@
-data "proxmox_virtual_environment_pools" "available_pools" {}
-
 output "root" {
-  value = data.proxmox_virtual_environment_pools.available_pools
+  value = "${file("/mnt/workspace/id_rsa")}"
 }

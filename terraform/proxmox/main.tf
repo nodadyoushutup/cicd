ebeb388ce9362863c9fbcf23b168739f78567726
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-11 17:09:11 -0500

    [WIP] Proxmox

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -27,8 +27,6 @@ resource "proxmox_virtual_environment_file" "cicd_cloud_config" {
             permissions: '0600'
             encoding: b64
             content: ${base64encode(data.local_file.ssh_private_key.content)}
-    mounts:
-        - ["${var.NAS_LOCAL_IP}:/mnt/epool/media", "/mnt/epool/media", "nfs", "defaults,nofail", "1000", "1000"]
     runcmd:
         - timedatectl set-timezone America/New_York
         - mkdir -p /mnt/epool/media

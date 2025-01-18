4dfc8f131c9c0af62722176031bded3e9d07931b
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 17:37:29 -0500

    [WIP] Jenkins

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,3 +1,8 @@
 resource "docker_image" "jenkins" {
   name = "ghcr.io/nodadyoushutup/jenkins:2.493"
 }
+
+resource "docker_container" "jenkins" {
+  name  = "jenkins"
+  image = docker_image.ubuntu.jenkins
+}

d8c6a07542a41897c3ac6e69b79bc0dc5e4d044b
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 14:05:45 -0500

    [WIP] Docker

--- terraform/docker/provider.tf
+++ terraform/docker/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "docker" {
-  host     = "ssh://${var.VIRTUAL_MACHINE_USERNAME}@${var.PROXMOX_VE_SSH_NODE_ADDRESS}:1022"
+  host     = "ssh://${var.VIRTUAL_MACHINE_USERNAME}@${var.PROXMOX_VE_SSH_NODE_ADDRESS}:10122"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-o", "IdentityFile=/mnt/workspace/id_rsa"]
 }

0c98b4cb3b491bcbae0b2961b710c158189fac3e
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 17:47:50 -0500

    [WIP] Docker

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -10,7 +10,7 @@ resource "docker_volume" "jenkins" {
 resource "docker_container" "jenkins" {
   depends_on = [docker_volume.jenkins]
   name  = "jenkins"
-  image = docker_image.jenkins
+  image = docker_image.jenkins.image_id
   ports {
     internal = "8080"
     external = "8080"

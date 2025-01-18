4939c75f101b2c2e3cd6b8ebf02881f5605c4344
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 13:57:43 -0500

    [WIP] Docker

---
+++ terraform/docker/variable.tf
@@ -0,0 +1,59 @@
+variable "PROXMOX_VE_USERNAME" {
+  type = string
+  default = null
+}
+
+variable "PROXMOX_VE_PASSWORD" {
+  type = string
+  default = null
+}
+
+variable "PROXMOX_VE_ENDPOINT" {
+  type = string
+  default = null
+}
+
+variable "PROXMOX_VE_SSH_NODE_ADDRESS" {
+  type = string
+  default = null
+}
+
+variable "PROXMOX_VE_SSH_NODE_NAME" {
+  type = string
+  default = null
+}
+
+variable "VIRTUAL_MACHINE_USERNAME" {
+  type = string
+  default = null
+}
+
+variable "VIRTUAL_MACHINE_PASSWORD" {
+  type = string
+  default = null
+}
+
+variable "SSH_PRIVATE_KEY" {
+  type = string
+  default = null
+}
+
+variable "SSH_PUBLIC_KEY" {
+  type = string
+  default = null
+}
+
+variable "GITCONFIG" {
+  type = string
+  default = null
+}
+
+variable "NAS_LOCAL_IP" {
+  type = string
+  default = null
+}
+
+variable "ISCSI_BASE_NAME" {
+  type = string
+  default = null
+}

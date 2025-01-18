546859b72177187b49cfe3f6456dd233cd026d97
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 14:41:57 -0500

    [WIP] Docker

--- terraform/docker/provider.tf
+++ terraform/docker/provider.tf
@@ -10,4 +10,10 @@ terraform {
 provider "docker" {
   host     = "ssh://${var.VIRTUAL_MACHINE_USERNAME}@${var.PROXMOX_VE_SSH_NODE_ADDRESS}:10122"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-o", "IdentityFile=/mnt/workspace/id_rsa"]
+
+  registry_auth {
+    address  = "https://index.docker.io/v1/"
+    username = "nodadyoushutup"
+    password = var.VIRTUAL_MACHINE_PASSWORD
+  }
 }

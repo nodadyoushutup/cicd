b37b21a7f427b34c81c87ace6d19f0d10612fa9d
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 17:44:13 -0500

    [WIP] Docker

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -2,7 +2,17 @@ resource "docker_image" "jenkins" {
   name = "ghcr.io/nodadyoushutup/jenkins:2.493"
 }
 
+resource "docker_volume" "jenkins" {
+  depends_on = [docker_image.jenkins]
+  name = "jenkins"
+}
+
 resource "docker_container" "jenkins" {
+  depends_on = [docker_volume.jenkins]
   name  = "jenkins"
   image = docker_image.ubuntu.jenkins
+  ports = {
+    internal = "8080"
+    external = "8080"
+  }
 }

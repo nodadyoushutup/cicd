3002c53f3c5ad23181664b417b9affddb29f14df
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 21:54:52 -0500

    [WIP] Jenkins

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -26,7 +26,7 @@ resource "null_resource" "create_remote_file" {
     inline = [
       "mkdir -p /home/ubuntu/init.groovy.d",
       "chown ubuntu:ubuntu /home/ubuntu/init.groovy.d",
-      "echo '${local.github_auth.content}' > /tmp/github_auth.groovy"
+      "echo '${local.github_auth}' > /tmp/github_auth.groovy"
       # "cp /tmp/github_auth.groovy /home/ubuntu/github_auth.groovy"
     ]
   }

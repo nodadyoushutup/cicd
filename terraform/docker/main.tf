c86b1f0474db345557e6aa346d5d80bf7541fe58
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 20:52:42 -0500

    [WIP] Jenkins

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,43 +1,43 @@
-# locals {
-#   java_opts = [
-#     "-Djenkins.install.runSetupWizard=false"
-#   ]
-# }
+locals {
+  java_opts = [
+    "-Djenkins.install.runSetupWizard=false"
+  ]
+}
 
-# resource "docker_image" "jenkins" {
-#   name = "ghcr.io/nodadyoushutup/jenkins:2.493"
-# }
+resource "docker_image" "jenkins" {
+  name = "ghcr.io/nodadyoushutup/jenkins:2.493"
+}
 
-# resource "docker_volume" "jenkins" {
-#   depends_on = [docker_image.jenkins]
-#   name = "jenkins"
-# }
+resource "docker_volume" "jenkins" {
+  depends_on = [docker_image.jenkins]
+  name = "jenkins"
+}
 
-# resource "docker_container" "jenkins" {
-#   depends_on = [docker_volume.jenkins]
-#   name  = "jenkins"
-#   image = docker_image.jenkins.image_id
-#   env = ["JAVA_OPTS=${join(" ", local.java_opts)}"]
-#   restart = "unless-stopped"
-#   start = true
+resource "docker_container" "jenkins" {
+  depends_on = [docker_volume.jenkins]
+  name  = "jenkins"
+  image = docker_image.jenkins.image_id
+  env = ["JAVA_OPTS=${join(" ", local.java_opts)}"]
+  restart = "unless-stopped"
+  start = true
   
-#   ports {
-#     internal = "8080"
-#     external = "8080"
-#   }
+  ports {
+    internal = "8080"
+    external = "8080"
+  }
 
-#   volumes {
-#     volume_name = docker_volume.jenkins.name
-#     container_path = "/var/jenkins_home"
-#   }
+  volumes {
+    volume_name = docker_volume.jenkins.name
+    container_path = "/var/jenkins_home"
+  }
 
-#   volumes {
-#     container_path = "/usr/share/jenkins/ref/init.groovy.d"
-#     host_path = "/home/ubuntu/init.groovy.d"
-#   }
+  volumes {
+    container_path = "/usr/share/jenkins/ref/init.groovy.d"
+    host_path = "/home/ubuntu/init.groovy.d"
+  }
 
-#   healthcheck {
-#     test = ["http://localhost:8080/whoAmI/api/json?tree=authenticated"]
-#   }
+  healthcheck {
+    test = ["http://localhost:8080/whoAmI/api/json?tree=authenticated"]
+  }
 
-# }
+}

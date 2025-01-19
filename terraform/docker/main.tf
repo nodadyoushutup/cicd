0582afe0f42dcf6ace22bf83105f36f4696eb82b
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-18 20:05:58 -0500

    [WIP] Jenkins

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,37 +1,37 @@
-# locals {
-#   java_opts = [
-#     "-Djenkins.install.runSetupWizard=false"
-#   ]
-# }
+locals {
+  java_opts = [
+    "-Djenkins.install.runSetupWizard=false"
+  ]
+}
 
-# resource "docker_image" "jenkins" {
-#   name = "ghcr.io/nodadyoushutup/jenkins:2.493"
-# }
+resource "docker_image" "jenkins" {
+  name = "ghcr.io/nodadyoushutup/jenkins:2.493"
+}
 
-# resource "docker_volume" "jenkins" {
-#   depends_on = [docker_image.jenkins]
-#   name = "jenkins"
-# }
+resource "docker_volume" "jenkins" {
+  depends_on = [docker_image.jenkins]
+  name = "jenkins"
+}
 
-# resource "docker_container" "jenkins" {
-#   depends_on = [docker_volume.jenkins]
-#   name  = "jenkins"
-#   image = docker_image.jenkins.image_id
-#   env = ["JAVA_OPTS=${join(" ", local.java_opts)}"]
+resource "docker_container" "jenkins" {
+  depends_on = [docker_volume.jenkins]
+  name  = "jenkins"
+  image = docker_image.jenkins.image_id
+  env = ["JAVA_OPTS=${join(" ", local.java_opts)}"]
   
-#   ports {
-#     internal = "8080"
-#     external = "8080"
-#   }
+  ports {
+    internal = "8080"
+    external = "8080"
+  }
 
-#   volumes {
-#     volume_name = docker_volume.jenkins.name
-#     container_path = "/var/jenkins_home"
-#   }
+  volumes {
+    volume_name = docker_volume.jenkins.name
+    container_path = "/var/jenkins_home"
+  }
 
-#   volumes {
-#     container_path = "/usr/share/jenkins/ref/init.groovy.d"
-#     host_path = "/home/ubuntu/init.groovy.d"
-#   }
+  volumes {
+    container_path = "/usr/share/jenkins/ref/init.groovy.d"
+    host_path = "/home/ubuntu/init.groovy.d"
+  }
 
-# }
+}

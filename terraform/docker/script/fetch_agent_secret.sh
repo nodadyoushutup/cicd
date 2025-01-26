a76e863ea0e44a2a8f523a26b8637c5cdda80543
nodadyoushutup <admin@nodadyoushutup.com>
2025-01-25 22:32:26 -0500

    [WIP] Jenkins

--- terraform/docker/script/fetch_agent_secret.sh
+++ terraform/docker/script/fetch_agent_secret.sh
@@ -6,8 +6,8 @@ HOST="$1"
 USER="$2"
 KEY="$3"
 
-SECRET_VALUE=$(ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$HOST" \
-    cat /home/ubuntu/secret/jenkins_agent 2>/dev/null)
+# SECRET_VALUE=$(ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$HOST" cat /home/ubuntu/secret/jenkins_agent 2>/dev/null)
+SECRET_VALUE=$(ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$HOST" ls -la /home/ubuntu 2>/dev/null)
 
 # Return in JSON format
 # echo "{\"secret\": \"${SECRET_VALUE}\"}"

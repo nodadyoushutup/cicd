af9a406dbc8671cf9a303144e78dff59674d3ffd
NoDadYouShutUp <70164187+nodadyoushutup@users.noreply.github.com>
2025-06-30 13:39:31 -0400

    Add agent entrypoint script

---
+++ init.groovy.d/export-agent-secret.groovy
@@ -0,0 +1,23 @@
+import jenkins.model.Jenkins
+
+def agentName = 'simple-agent'
+def secretFile = new File('/run/secrets/jenkins-agent-secret')
+
+try {
+    def node = Jenkins.instance.getNode(agentName)
+    if (node != null) {
+        def computer = node.toComputer()
+        if (computer != null) {
+            def secret = computer.jnlpMac
+            secretFile.parentFile.mkdirs()
+            secretFile.text = secret
+            println "Generated secret for '${agentName}' at ${secretFile}"
+        } else {
+            println "No computer for node '${agentName}'"
+        }
+    } else {
+        println "Node '${agentName}' not found"
+    }
+} catch (Throwable t) {
+    println "Error writing agent secret: ${t.message}"
+}
